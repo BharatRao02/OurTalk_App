d07e7f942e2d68ab48c996364ec55a96ad0072a8
Bharat <Bharatrao889@gmail.com>
2023-09-20 21:41:02 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, (err) => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://localhost:3000/",
+    origin: "https://localhost:3000",
   },
 });
 
